5dac3f1c762c9be4660b5ffeb7f123cb2f794541
JanReisiegel <jan.reisiegel@pslib.cz>
2022-05-16 21:59:34 +0200

    drobná úprava vzhledu

--- SobotySTechnikou/SobotySTechnikou/ClientApp/src/components/Home.js
+++ SobotySTechnikou/SobotySTechnikou/ClientApp/src/components/Home.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Edit } from '@rsuite/icons/lib/icons';
 import axios from 'axios';
 import ActionOnHome from './ActionsComponents/ActionOnHome';
+import Unauthorized from './general/Unauthorized';
 
 export const Home = () => {
   const [{ userManager, accessToken, profile }] = useAuthContext();
@@ -13,15 +14,15 @@ export const Home = () => {
   const [error, setError] = useState(false);
   console.log(profile)  
     return (
-      <div className='text-center'>
+      <div /*className='text-center'*/>
         {
           accessToken ?
-          !profile.completed_information ?
-            <Message type="error" header="Nevyplněné informace" >
+          profile.completed_information ?
+            <Message style={{textAlign: "center"}} type="error" header="Nevyplněné informace" >
               <p>Nemůžete se přihlásit na jakoukoli akci, protože nemáte vyplněné nějaké důležité informace</p>
               <IconButton color='red' as={Link} to={"/EditUser"} icon={<Edit />} appearance='ghost' >Doplnit informace</IconButton>
             </Message>
-            : null : null
+            : null : <Unauthorized redirectToUrl={"/"}/>
         }
         <ActionOnHome />
         
